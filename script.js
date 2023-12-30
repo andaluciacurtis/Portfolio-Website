cc2dd356349adc722953e2d049e4e39c338ca5b5
andaluciacurtis <andalucia.curtis@gmail.com>
2023-12-29 18:00:47 -0800

    added speech bubble for each project when clicked

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let projects = document.querySelectorAll('.project');
+let triangles = document.querySelectorAll('.triangle');
+
 for (let i = 0; i < projects.length; i++) {
   let currentProject = projects[i];
   currentProject.addEventListener("click", ()=> {
     currentProject.classList.add("selected");
+    triangles[i].style.visibility = "visible";
     for (let j = 0; j < projects.length; j++) {
       if (currentProject != projects[j]) {
         projects[j].classList.remove("selected");
+        triangles[j].style.visibility = "hidden";
       }
     }
   })
