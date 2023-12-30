2df7ef8db42b0beb273269589ddaa45f01f71d2d
andaluciacurtis <andalucia.curtis@gmail.com>
2023-12-29 20:12:46 -0800

    creating project carousel

--- script.js
+++ script.js
@@ -20,20 +20,20 @@ pageOneObserver.observe(pageOne);
 
 
 
-const projects = document.querySelectorAll('.project');
-const triangles = document.querySelectorAll('.triangle');
+// const projects = document.querySelectorAll('.project');
+// const triangles = document.querySelectorAll('.triangle');
 
-for (let i = 0; i < projects.length; i++) {
-  let currentProject = projects[i];
-  currentProject.addEventListener("click", ()=> {
-    currentProject.classList.add("selected");
-    triangles[i].style.visibility = "visible";
-    for (let j = 0; j < projects.length; j++) {
-      if (currentProject != projects[j]) {
-        projects[j].classList.remove("selected");
-        triangles[j].style.visibility = "hidden";
-      }
-    }
-  })
-}
+// for (let i = 0; i < projects.length; i++) {
+//   let currentProject = projects[i];
+//   currentProject.addEventListener("click", ()=> {
+//     currentProject.classList.add("selected");
+//     triangles[i].style.visibility = "visible";
+//     for (let j = 0; j < projects.length; j++) {
+//       if (currentProject != projects[j]) {
+//         projects[j].classList.remove("selected");
+//         triangles[j].style.visibility = "hidden";
+//       }
+//     }
+//   })
+// }
 
