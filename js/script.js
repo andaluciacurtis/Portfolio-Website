fc9cfaa3771065f2450411ddf366851a04e1f2cf
andaluciacurtis <andalucia.curtis@gmail.com>
2024-01-07 20:11:09 -0800

    added working mobile nav bar

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
+// ADDING NAV INTERSECTION OBSERVERS
 const navBar = document.querySelector('nav');
+
+let isPageOne = true;
 
 const pageOneObserver = new IntersectionObserver(function(entries) {
   entries.forEach(entry=> {
     if (!entry.isIntersecting) {
       navBar.classList.add("show");
+      isPageOne = false;
     } else {
       navBar.classList.remove("show");
+      isPageOne = true;
+
+      // Making sure the mobile nav links + dimmer disappear when the nav does
+      if (navOpen) {
+        openCloseNav();
+      }
     }
   })
 }, 
@@ -32,3 +42,31 @@ const pageThreeObserver = new IntersectionObserver(function(entries) {
 );
 
 pageThreeObserver.observe(pageThree);
+
+// CREATING MOBILE NAV
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const mobileNav = document.querySelector(".mobile-nav-container");
+const bgDimmer = document.querySelector(".mobile-bg-dimmer");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+let navOpen = false;
+
+navToggle.addEventListener("click", ()=> {
+  if (!isPageOne) {
+    openCloseNav();
+  }
+})
+
+// Adding listener for each link, so when clicked, the nav will go away
+navLinks.forEach(link=> {
+  link.addEventListener("click", ()=> {
+    openCloseNav();
+  })
+})
+
+function openCloseNav() {
+  navOpen = !navOpen;
+  mobileNav.classList.toggle("nav-show");
+  bgDimmer.style.transition = "opacity 0.5s ease-in-out";
+  bgDimmer.classList.toggle("dimmer-show");
+}
